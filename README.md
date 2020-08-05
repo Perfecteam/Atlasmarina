fdb009717c3300a68d7f3d2cdb630f4a054c9d07
Hager-Ali <hagerali19983@gmail.com>
2020-08-05 15:05:15 +0200

    edit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Atlasmarina-services
---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 200) {
+          $(".fixed-top").css("background" , "#2e2e2e");
+          $('.logo').css('display', 'none');
+          $('.logo-text').css('display', 'inline');
+        }
+  
+        else{
+            $(".fixed-top").css("background" , "transparent");
+            $('.logo').css('display', 'inline');
+            $('.logo-text').css('display', 'none');
+        }
+    });
+  });
